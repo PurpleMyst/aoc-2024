cf8608f29470fb7119b3500614e238700e4443dd
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-12-04 12:01:59 +0100

    perf(day04): rayon

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -1,41 +1,20 @@
 use std::fmt::Display;
 
+use grid::Grid;
+
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
     let input = include_str!("input.txt");
-    let grid = grid::Grid::from_vec(
+    let grid = Grid::from_vec(
         input.bytes().filter(|&b| b != b'\n').collect(),
         input.lines().next().unwrap().len(),
     );
 
-    let p1 = grid
-        .indexed_iter()
-        .filter(|(_, &c)| c == b'X')
-        .map(|((y, x), _)| {
-            let options: [[_; 3]; 8] = [
-                std::array::from_fn(|i| (Some(y), x.checked_sub(i + 1))),
-                std::array::from_fn(|i| (Some(y), x.checked_add(i + 1))),
-                std::array::from_fn(|i| (y.checked_sub(i + 1), Some(x))),
-                std::array::from_fn(|i| (y.checked_add(i + 1), Some(x))),
-                std::array::from_fn(|i| (y.checked_sub(i + 1), x.checked_sub(i + 1))),
-                std::array::from_fn(|i| (y.checked_add(i + 1), x.checked_add(i + 1))),
-                std::array::from_fn(|i| (y.checked_sub(i + 1), x.checked_add(i + 1))),
-                std::array::from_fn(|i| (y.checked_add(i + 1), x.checked_sub(i + 1))),
-            ];
+    rayon::join(|| solve_part1(&grid), || solve_part2(&grid))
+}
 
-            options
-                .into_iter()
-                .filter(|path| {
-                    path.iter()
-                        .zip(b"MAS")
-                        .all(|((y, x), c)| y.zip(*x).and_then(|(y, x)| grid.get(y, x)) == Some(c))
-                })
-                .count()
-        })
-        .sum::<usize>();
-
-    let p2 = grid
-        .indexed_iter()
+fn solve_part2(grid: &Grid<u8>) -> usize {
+    grid.indexed_iter()
         .filter(|(_, &c)| c == b'A')
         .filter(|((y, x), _)| {
             let first_diagonal = [
@@ -62,7 +41,32 @@ pub fn solve() -> (impl Display, impl Display) {
 
             do_count(first_diagonal) == (1, 1) && do_count(second_diagonal) == (1, 1)
         })
-        .count();
+        .count()
+}
 
-    (p1, p2)
+fn solve_part1(grid: &Grid<u8>) -> usize {
+    grid.indexed_iter()
+        .filter(|(_, &c)| c == b'X')
+        .map(|((y, x), _)| {
+            let options: [[_; 3]; 8] = [
+                std::array::from_fn(|i| (Some(y), x.checked_sub(i + 1))),
+                std::array::from_fn(|i| (Some(y), x.checked_add(i + 1))),
+                std::array::from_fn(|i| (y.checked_sub(i + 1), Some(x))),
+                std::array::from_fn(|i| (y.checked_add(i + 1), Some(x))),
+                std::array::from_fn(|i| (y.checked_sub(i + 1), x.checked_sub(i + 1))),
+                std::array::from_fn(|i| (y.checked_add(i + 1), x.checked_add(i + 1))),
+                std::array::from_fn(|i| (y.checked_sub(i + 1), x.checked_add(i + 1))),
+                std::array::from_fn(|i| (y.checked_add(i + 1), x.checked_sub(i + 1))),
+            ];
+
+            options
+                .into_iter()
+                .filter(|path| {
+                    path.iter()
+                        .zip(b"MAS")
+                        .all(|((y, x), c)| y.zip(*x).and_then(|(y, x)| grid.get(y, x)) == Some(c))
+                })
+                .count()
+        })
+        .sum::<usize>()
 }
