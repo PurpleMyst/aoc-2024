de74c469d402ab4a3934bb67502517d6203f1a0c
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-12-06 17:03:24 +0100

    refactor(day06): avoid labels

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -12,7 +12,14 @@ pub fn solve() -> (impl Display, impl Display) {
         input.lines().next().unwrap().len(),
     );
 
-    let p1 = do_solve(&map).0.into_vec().into_iter().filter(|&v| v != 0).count();
+    let (start_pos, _) = map.indexed_iter().find(|&(_, &c)| c == b'^').unwrap();
+
+    let p1 = do_solve(&map, start_pos)
+        .0
+        .into_vec()
+        .into_iter()
+        .filter(|&v| v != 0)
+        .count();
 
     let p2 = map
         .indexed_iter()
@@ -21,7 +28,7 @@ pub fn solve() -> (impl Display, impl Display) {
         .filter(|(pos, _)| {
             let mut new_map = map.clone();
             new_map[*pos] = b'#';
-            let (_, enters_loop) = do_solve(&new_map);
+            let (_, enters_loop) = do_solve(&new_map, start_pos);
             enters_loop
         })
         .count();
@@ -29,33 +36,27 @@ pub fn solve() -> (impl Display, impl Display) {
     (p1, p2)
 }
 
-fn do_solve(map: &grid::Grid<u8>) -> (grid::Grid<u8>, bool) {
+fn do_solve(map: &grid::Grid<u8>, mut pos: (usize, usize)) -> (grid::Grid<u8>, bool) {
     let mut visited = grid::Grid::new(map.rows(), map.cols());
     visited.fill(0u8);
 
-    let (mut pos, _) = map.indexed_iter().find(|&(_, &c)| c == b'^').unwrap();
-
-    let mut dir = 0usize;
-
-    'mainloop: loop {
-        let mask = 1 << dir;
-        if visited[pos] & mask != 0 {
-            return (visited, true);
-        }
-        visited[pos] |= mask;
-        pos = loop {
-            let new_pos = pos
-                .0
-                .checked_add_signed(DIRECTIONS[dir].0)
-                .zip(pos.1.checked_add_signed(DIRECTIONS[dir].1));
-            match new_pos.and_then(|p| map.get(p.0, p.1)) {
-                Some(b'#') => {
-                    dir = (dir + 1) % 4;
+    loop {
+        for (dir, (dy, dx)) in DIRECTIONS.into_iter().enumerate() {
+            let mask = 1 << dir;
+            loop {
+                if visited[pos] & mask != 0 {
+                    return (visited, true);
                 }
-                Some(..) => break new_pos.unwrap(),
-                None => break 'mainloop,
+                visited[pos] |= mask;
+                let new_pos = pos.0.checked_add_signed(dy).zip(pos.1.checked_add_signed(dx));
+                match new_pos.and_then(|p| map.get(p.0, p.1)) {
+                    Some(b'#') => {
+                        break;
+                    }
+                    Some(..) => pos = new_pos.unwrap(),
+                    None => return (visited, false),
+                }
             }
         }
     }
-    (visited, false)
 }
