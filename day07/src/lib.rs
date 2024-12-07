ac127f610c8af2b71d1631893a2df9fc3fb31fb9
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-12-07 12:55:10 +0100

    perf(day07): implement backtracking solution from reddit megathread

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,59 +1,42 @@
-use std::{fmt::Display, mem::swap};
+use std::fmt::Display;
 
 use rayon::prelude::*;
-
-fn concat(a: u64, ob: u64) -> u64 {
-    let mut b = ob;
-    let mut c = 1;
-    while b > 0 {
-        c *= 10;
-        b /= 10;
-    }
-    a * c + ob
-}
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
     let input = include_str!("input.txt");
 
-    rayon::join(
-        || do_solve(input, |state, v| [state + v, state * v]),
-        || do_solve(input, |state, v| [state + v, state * v, concat(state, v)]),
-    )
+    rayon::join(|| do_solve::<false>(input), || do_solve::<true>(input))
 }
 
-fn do_solve<F, I>(input: &str, f: F) -> u64
-where
-    F: Fn(u64, u64) -> I + Sync,
-    I: IntoIterator<Item = u64>,
-{
+fn ends_with(a: u64, b: u64) -> bool {
+    b <= a && (a - b) % 10u64.pow(b.ilog10() + 1) == 0
+}
+
+// Solution translated from:
+// https://www.reddit.com/r/adventofcode/comments/1h8l3z5/2024_day_7_solutions/m0tv6di/
+fn is_tractable<const PART2: bool>(target: u64, values: &[u64]) -> bool {
+    let (&n, head) = values.split_last().unwrap();
+    if head.is_empty() {
+        return n == target;
+    }
+
+    let q = target / n;
+    let r = target % n;
+
+    (r == 0 && is_tractable::<PART2>(q, head))
+        || (PART2 && ends_with(target, n) && is_tractable::<PART2>(target / (10u64.pow(n.ilog10() + 1)), head))
+        || (n <= target && is_tractable::<PART2>(target - n, head))
+}
+
+fn do_solve<const PART2: bool>(input: &str) -> u64 {
     input
         .par_lines()
         .filter_map(|line| {
             let (target, values) = line.split_once(": ").unwrap();
             let target = target.parse::<u64>().unwrap();
-            let mut values = values.split(' ').map(|v| v.parse::<u64>().unwrap());
-
-            let mut states = vec![values.next().unwrap()];
-            let mut next_states = Vec::new();
-
-            for v in values {
-                next_states.clear();
-                next_states.extend(states.drain(..).flat_map(|state| f(state, v)).filter(|&s| s <= target));
-                swap(&mut states, &mut next_states);
-            }
-
-            states.into_iter().any(|s| s == target).then_some(target)
+            let values = values.split(' ').map(|v| v.parse::<u64>().unwrap()).collect::<Vec<_>>();
+            is_tractable::<PART2>(target, &values).then_some(target)
         })
         .sum::<u64>()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_concat() {
-        assert_eq!(concat(12, 345), 12345);
-    }
 }
