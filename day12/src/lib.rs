2c829be53da3eb205a7d1fe2898e0371f4ea067f
PurpleMyst <PurpleMyst@users.noreply.github.com>
2024-12-13 03:32:39 +0100

    chore: remove crud

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,6 +1,17 @@
 use std::fmt::Display;
 
-use owo_colors::OwoColorize;
+const DIRS: [(isize, isize); 8] = [
+    // first four are horizontal
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0),
+    // next four are diagonal
+    (1, 1),
+    (1, -1),
+    (-1, 1),
+    (-1, -1),
+];
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
@@ -48,8 +59,6 @@ fn do_solve(input: &str) -> (i32, i32) {
     let mut distinct_regions = regions.clone();
     distinct_regions.sort();
     distinct_regions.dedup();
-    // sort by letter
-    // distinct_regions.sort_by_key(|&r0| map.iter().zip(&regions).filter(|&(_, r)| r0 == *r).map(|(&b, _)| b).min().unwrap());
 
     let mut p1 = 0;
     let mut p2 = 0;
@@ -59,7 +68,6 @@ fn do_solve(input: &str) -> (i32, i32) {
         let mut perimeter = 0;
 
         let start_idx = (0..side * side).find(|&idx| regions[idx] == region).unwrap();
-        let (start_y, start_x) = coords(start_idx);
 
         let mut stack = vec![start_idx];
         let mut seen = vec![false; side * side];
@@ -87,12 +95,6 @@ fn do_solve(input: &str) -> (i32, i32) {
             }
         }
 
-        // let (mut cy, mut cx) = (start_y as isize - 1, start_x as isize);
-        //
-        // let (mut dy, mut dx) = (0, 1);
-        //
-        // let mut seen = HashSet::new();
-        //
         let mut region_min_y = usize::MAX;
         let mut region_max_y = 0;
         let mut region_min_x = usize::MAX;
@@ -105,123 +107,7 @@ fn do_solve(input: &str) -> (i32, i32) {
                 region_max_x = region_max_x.max(x);
             }
         }
-        //
-        // // hole = point not in region surrounded by regionn
-        // let mut holes =  0;
-        // for (y, x) in (0..side).flat_map(|y| (0..side).map(move |x| (y, x))) {
-        //     if regions[idx(y, x)] != region {
-        //         (-1..=1)
-        //             .flat_map(|dy| (-1..=1).map(move |dx| (dy, dx)))
-        //             .filter(|&(dy, dx)| (dy, dx) != (0, 0))
-        //             .all(|(dy, dx)| {
-        //                 let ny = y as isize + dy;
-        //                 let nx = x as isize + dx;
-        //                 (0..side as isize).contains(&ny) && (0..side as isize).contains(&nx) &&
-        //                 regions[idx(ny as usize, nx as usize)] == region
-        //             }).then(|| holes += 1);
-        //     }
-        // }
-        //     let mut sides = 0;
-        //
-        // loop {
-        //     // println!("  pos=({cy:2}, {cx:2}) dir={:?} cell={:?} sides={sides:2}", 
-        //     //     match (dy, dx) {
-        //     //         (0, 1) => "R",
-        //     //         (1, 0) => "D",
-        //     //         (0, -1) => "L",
-        //     //         (-1, 0) => "U",
-        //     //         _ => unreachable!(),
-        //     //     },
-        //     //     ((0..side as isize).contains(&cy) && (0..side as isize).contains(&cx)).then(||
-        //     //     map.get(idx(cy as usize, cx as usize)).map(|&b| b as char)).flatten());
-        //
-        //     println!("{sides:4}");
-        //     for y in region_min_y as isize - 1..=region_max_y as isize + 1 {
-        //         for x in region_min_x as isize -1..=region_max_x as isize + 1 {
-        //             if (y, x) == (cy, cx) {
-        //                 print!("\\x1b[31m{}\\x1b[0m", match (dy,dx) {
-        //                     (0, 1) => '>',
-        //                     (1, 0) => 'v',
-        //                     (0, -1) => '<',
-        //                     (-1, 0) => '^',
-        //                     _ => unreachable!(),
-        //                 });
-        //                 continue;
-        //             }
-        //
-        //             let c = if 
-        //
-        //                 (0..side as isize).contains(&y) && (0..side as isize).contains(&x) 
-        //                 && regions[idx(y as usize, x as usize)] == region
-        //             {
-        //                 map[idx(y as usize, x as usize)] as char
-        //             } else {
-        //                 '.'
-        //             };
-        //             print!("{}", c);
-        //         }
-        //         println!();
-        //     }
-        //     println!();
-        //
-        //     assert!(seen.insert((cy, cx, dy, dx)));
-        //
-        //     let right_dy = dx;
-        //     let right_dx = -dy;
-        //
-        //     let left_dy = -dx;
-        //     let left_dx = dy;
-        //
-        //     let can_move = |dy: isize, dx: isize|
-        //         (!(0..side as isize).contains(&(cy + dy)) ||
-        //         !(0..side as isize).contains(&(cx + dx)) ||
-        //         regions[idx((cy + dy) as usize, (cx + dx) as usize)] != region)
-        //          && /* check if there's at least one neighbor within the region */
-        //           (-1..=1).flat_map(|dy| (-1..=1).map(move |dx| (dy, dx)))
-        //             .filter(|&(dy, dx)| (dy, dx) != (0, 0))
-        //             .any(|(dy, dx)| {
-        //                 let ny = cy + dy;
-        //                 let nx = cx + dx;
-        //                 (0..side as isize).contains(&ny) && (0..side as isize).contains(&nx) &&
-        //                 regions[idx(ny as usize, nx as usize)] == region
-        //             })
-        //         ;
-        //
-        //     if can_move(right_dy, right_dx) {
-        //         dy = right_dy;
-        //         dx = right_dx;
-        //         sides += 1;
-        //     } else if can_move(dy, dx) {
-        //         // continue straight
-        //     } else {
-        //         dy = left_dy;
-        //         dx = left_dx;
-        //         sides += 1;
-        //         continue;
-        //     }
-        //     cy += dy;
-        //     cx += dx;
-        //
-        //     if cy == start_y as isize - 1 && cx == start_x as isize {
-        //         break;
-        //     }
-        // }
-
-        // bitset method
         let mut sides = 0;
-
-        const DIRS: [(isize, isize); 8] = [
-            // first four are horizontal
-            (0, 1),
-            (1, 0),
-            (0, -1),
-            (-1, 0),
-            // next four are diagonal
-            (1, 1),
-            (1, -1),
-            (-1, 1),
-            (-1, -1),
-        ];
 
         let mut bitset = vec![0u64; (1 + side * 2) * (1 + side * 2)];
         for y in 0..side {
@@ -230,8 +116,8 @@ fn do_solve(input: &str) -> (i32, i32) {
                     continue;
                 }
                 for (i, (dy, dx)) in DIRS.iter().copied().enumerate() {
-                    let ny = 1+(2*y as isize + dy);
-                    let nx = 1+(2*x as isize + dx);
+                    let ny = 1 + (2 * y as isize + dy);
+                    let nx = 1 + (2 * x as isize + dx);
                     assert!((0..=side as isize * 2).contains(&ny));
                     assert!((0..=side as isize * 2).contains(&nx));
 
@@ -240,75 +126,29 @@ fn do_solve(input: &str) -> (i32, i32) {
             }
         }
 
-        // draw regular grid
-        for y in region_min_y..=region_max_y {
-            for x in region_min_x..=region_max_x {
-                let i = idx(y, x);
-                if regions[i] == region {
-                    print!("{}", map[i] as char);
-                } else {
-                    print!("{}", ".".dimmed());
-                }
-            }
-            println!();
-        }
-
-        // draw expanded grid with bitset
-        // for y in 0..=side*2 {
-        //     for x in 0..=side*2 {
         for y in 2 * (region_min_y as isize)..=2 * (region_max_y as isize + 1) {
             for x in 2 * (region_min_x as isize)..=2 * (region_max_x as isize + 1) {
                 let y = y as usize;
                 let x = x as usize;
 
                 if y % 2 == 1 && x % 2 == 1 {
-                    let i = idx(y / 2, x / 2);
-                    if regions[i] == region {
-                        print!("{}", map[i] as char);
-                    } else {
-                        print!("{}", ".".dimmed());
-                    }
                 } else {
                     // this is a boundary point
                     let flags = bitset[y * (1 + side * 2) + x];
-                    if flags & 0xf0 != 0 && flags & 0x0f == 0 
-                        && (flags & 0xf0).count_ones() % 2 == 1 
-                    {
-                        print!("{}", "*".red());
+                    if flags & 0xf0 != 0 && flags & 0x0f == 0 && (flags & 0xf0).count_ones() % 2 == 1 {
                         sides += 1;
-                    } else if flags & 0b1001_0000 == 0b1001_0000 
-                        && flags & 0b0110_0000 == 0
-                        && flags & 0x0f == 0 {
-                        print!("{}", "*".green());
+                    } else if flags & 0b1001_0000 == 0b1001_0000 && flags & 0b0110_0000 == 0 && flags & 0x0f == 0 {
                         sides += 2;
-                    } else if flags & 0b0110_0000 == 0b0110_0000 
-                        && flags & 0b1001_0000 == 0
-                        && flags & 0x0f == 0 {
-                        print!("{}", "*".blue());
+                    } else if flags & 0b0110_0000 == 0b0110_0000 && flags & 0b1001_0000 == 0 && flags & 0x0f == 0 {
                         sides += 2;
-                    } else {
-                        // print!("{}", flags.count_ones().dimmed());
-                        print!("{}", ".".dimmed());
                     }
                 }
             }
-            println!();
         }
-        println!();
-
-
-        println!("Region {}:", (map[start_idx] as char).bold());
-        println!("  area      = {:3}", area.red());
-        println!("  perimeter = {:3}", perimeter.green());
-        println!("  sides     = {:3}", sides.blue());
-        // sides += 4 * holes;
-
 
         p1 += area * perimeter;
         p2 += area * sides;
     }
-
-    debug_assert_ne!(p2, 845490);
 
     (p1, p2)
 }
